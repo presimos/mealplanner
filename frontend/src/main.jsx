4d358a78a486f989cc2ccc6993e7faac976ef024
presimos <presimos@gmail.com>
2026-05-23 19:53:37 +0300

    init front

---
+++ frontend/src/main.jsx
@@ -0,0 +1,43 @@
+// main.jsx - Точка входа React
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
+import App from './App';
+import { AuthProvider } from './contexts/AuthContext';
+import { MealPlanProvider } from './contexts/MealPlanContext';
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <AuthProvider>
+        <MealPlanProvider>
+          <App />
+          <Toaster 
+            position="top-right"
+            toastOptions={{
+              duration: 3000,
+              style: {
+                background: '#363636',
+                color: '#fff',
+              },
+              success: {
+                iconTheme: {
+                  primary: '#22c55e',
+                  secondary: '#fff',
+                },
+              },
+              error: {
+                iconTheme: {
+                  primary: '#ef4444',
+                  secondary: '#fff',
+                },
+              },
+            }}
+          />
+        </MealPlanProvider>
+      </AuthProvider>
+    </BrowserRouter>
+  </React.StrictMode>
+);
